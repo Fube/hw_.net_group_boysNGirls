520a51223b363d8dbff3664595de469d88288f6b
Fube <fubeitch@gmail.com>
2021-10-08 17:43:51 -0400

    Added student service

---
+++ BoysNGirls/Models/Tasks/StudentTaskService.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+
+namespace BoysNGirls.Models.Tasks
+{
+    public class StudentTaskService : ITaskService
+    {
+        public List<Task> GetTasks(int id)
+        {
+            //Return hardcoded list containing tasks for students
+            return new List<Task>
+            {
+                new Task
+                {
+                    Code="200B10",
+                    Description = "Attend classes",
+                    Location = "F-249"
+                },
+                new Task
+                {
+                    Code="200B11",
+                    Description = "Do some tutoring",
+                    Location = "A-103"
+                },
+                new Task
+                {
+                    Code="200B12",
+                    Description = "Make some friends",
+                    Location = "College"
+                },
+                new Task
+                {
+                    Code="200B13",
+                    Description = "Score good grades",
+                    Location = "Department"
+                }
+            };
+
+        }
+    }
+}
--- BoysNGirls/Models/Tasks/Task.cs
+++ BoysNGirls/Models/Tasks/Task.cs
@@ -2,9 +2,9 @@ namespace BoysNGirls.Models.Tasks
 {
     public class Task
     {
-        public string Code { get; set; }
+        // Mutators and accessors  are given using the get and set keywords
         public string Description { get; set; }
-
+        public string Code { get; set; }
         public string Location { get; set; }
     }
 }
