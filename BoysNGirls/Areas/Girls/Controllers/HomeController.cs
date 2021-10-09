759e69b825a1dd7be8cd7ec37e341f6fd610683d
Fube <fubeitch@gmail.com>
2021-10-09 16:59:11 -0400

    Done with assignment 2

--- BoysNGirls/Areas/Girls/Controllers/HomeController.cs
+++ BoysNGirls/Areas/Girls/Controllers/HomeController.cs
@@ -1,17 +1,24 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BoysNGirls.Models.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BoysNGirls.Areas.Girls.Controllers
 {
     [Area("Girls")]
     public class HomeController : Controller
     {
+        private ITaskService _taskService;
+
+        // Inject task service through constructor
+        public HomeController(ITaskService taskService)
+        {
+            _taskService = taskService;
+        }
         public IActionResult Index()
         {
             ViewBag.Message = "There are 2 girls in the class";
+            ViewBag.Tasks = string.Equals(_taskService.GetType(), "student", StringComparison.OrdinalIgnoreCase) ? _taskService.GetTasks() : new List<Task>();
             return View();
         }
     }
--- BoysNGirls/Startup.cs
+++ BoysNGirls/Startup.cs
@@ -1,3 +1,4 @@
+using BoysNGirls.Models.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -13,17 +14,32 @@ namespace BoysNGirls
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        private IWebHostEnvironment _env;
+        public Startup(IWebHostEnvironment env)
         {
-            Configuration = configuration;
+            {
+                //Configuration = configuration;
+                _env = env;
+            }
         }
-
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            // When in development mode, use TeacherTaskService
+            // When in production mode, use StudentTaskService
+            if (_env.IsDevelopment())
+            {
+                // Transient because I don't want to risk thread collisions
+                services.AddTransient<ITaskService, TeacherTaskService>();
+            }
+            else if (_env.IsProduction())
+            {
+                services.AddTransient<ITaskService, StudentTaskService>();
+            }
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -48,9 +64,12 @@ namespace BoysNGirls
 
             app.UseEndpoints(endpoints =>
             {
+                // Given that the area exists, this will route to the area
+                // Since we are routing by convention, this will route to whatever is in the Area folder
                 endpoints.MapControllerRoute(
                     name: "areas",
                     pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+                // Otherwise, this will route to the default (root) area
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
